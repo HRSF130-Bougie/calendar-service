6b57a5c0eaed630b7bcfdbcbfe11f190aeb0dad8
Elana Cohen-Khani <elanadev@gmail.com>
2020-09-25 00:31:35 -0700

    Create seed data

--- server/router.js
+++ server/router.js
@@ -1,12 +1,23 @@
 const router = require('express').Router();
 const db = require('./database.js');
-const Listing = require('./schema.js');
+const schema = require('./schema.js');
 
-// Get request
+// Get request from the client
 router.route('/api/listings').get((req, res) => {
-  Listing.find()
-    .then((listings) => { console.log(listings); res.status(200).send(listings) })
+  schema.Listing.find()
+    .then((listings) => { console.log(listings); res.status(200).send(listings); })
     .catch((err) => res.status(400).send(`Error: ${err}`));
+});
+
+// Get request from postman to re-seed
+router.route('/api/reseed').get((req, res) => {
+  schema.reSeed((error, success) => {
+    if (error) {
+      res.status(400).send(`Something went wrong ${error}`);
+    } else {
+      res.status(200).send('Woohoo! ');
+    }
+  });
 });
 
 module.exports = router;
--- server/schema.js
+++ server/schema.js
@@ -1,4 +1,7 @@
+/* eslint-disable no-console */
 const mongoose = require('mongoose');
+const dayjs = require('dayjs');
+const faker = require('faker');
 
 const { Schema } = mongoose;
 
@@ -29,4 +32,51 @@ const listingSchema = new Schema({
 
 const Listing = mongoose.model('Listing', listingSchema);
 
-module.exports = Listing;
+const reSeed = async (callback) => {
+  try {
+    // Delete existing documents
+    await Listing.deleteMany({});
+
+    // Create 100 new records
+    for (let listCount = 1; listCount <= 100; listCount += 1) {
+      const daysArray = [];
+
+      for (let dayCount = 0; dayCount <= 182; dayCount += 1) {
+        // Construct day object to be pushed to array, 6 months worth of days
+        const day = {
+          date: dayjs().startOf('month').add(dayCount, 'day').toDate(),
+          booked: faker.random.boolean(),
+          price: 100,
+          minimumNights: 1,
+        };
+
+        // Make weekends more expensive
+        if (day.date.getDay() >= 5) {
+          day.price = 120;
+        }
+
+        // Make a two day minimum on Fridays
+        if (day.date.getDay() === 5) {
+          day.minimumNights = 2;
+        }
+        daysArray.push(day);
+      }
+
+      const newListing = new Listing({
+        listing_id: listCount,
+        days: daysArray,
+      });
+
+      newListing.save();
+    }
+  } catch (error) {
+    console.error(error);
+    callback(error);
+  } finally {
+    callback(null, 'new records created');
+  }
+};
+
+module.exports = {
+  Listing, reSeed,
+};
