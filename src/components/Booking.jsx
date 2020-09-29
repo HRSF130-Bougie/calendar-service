3fe8c364a453b6fb7335a01b9b8c4ac685be5ba2
Elana Cohen-Khani <elanadev@gmail.com>
2020-09-29 00:00:23 -0700

    All necessary text present for widget header

--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
 import Calendar from './Calendar';
+import Widget from './Widget';
 
 class Booking extends React.Component {
   constructor() {
@@ -14,6 +15,7 @@ class Booking extends React.Component {
     return (
       <>
         <h1>Hello World from Inside the Booking Component! Testing Again!</h1>
+        <Widget />
         <Calendar />
       </>
     );
