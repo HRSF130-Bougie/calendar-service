ef730b8d56beea26f387700013d80699b501616f
Elana Cohen-Khani <elanadev@gmail.com>
2020-09-30 20:49:38 -0700

    Refactor font declarations to be in their own files

--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -3,65 +3,7 @@ import { hot } from 'react-hot-loader/root';
 import { createGlobalStyle } from 'styled-components';
 import Calendar from './Calendar';
 import Widget from './Widget/Widget';
-
-import AirbnbCerealBlack from '../assets/typefaces/AirbnbCerealBlack.woff';
-import AirbnbCerealBold from '../assets/typefaces/AirbnbCerealBold.woff';
-import AirbnbCerealBook from '../assets/typefaces/AirbnbCerealBook.woff';
-import AirbnbCerealExtraBold from '../assets/typefaces/AirbnbCerealExtraBold.woff';
-import AirbnbCerealLight from '../assets/typefaces/AirbnbCerealLight.woff';
-import AirbnbCerealMedium from '../assets/typefaces/AirbnbCerealMedium.woff';
-
-const GlobalStyle = createGlobalStyle`
-    @font-face {
-      font-family: 'Airbnb Cereal App Book';
-      font-style: normal;
-      font-weight: normal;
-      src: local('Airbnb Cereal App Book'), url(${AirbnbCerealBook}) format('woff');
-    }
-
-    @font-face {
-      font-family: 'Airbnb Cereal App Light';
-      font-style: normal;
-      font-weight: normal;
-      src: local('Airbnb Cereal App Light'), url(${AirbnbCerealLight}) format('woff');
-    }
-
-    @font-face {
-      font-family: 'Airbnb Cereal App Medium';
-      font-style: normal;
-      font-weight: normal;
-      src: local('Airbnb Cereal App Medium'), url(${AirbnbCerealMedium}) format('woff');
-    }
-
-    @font-face {
-      font-family: 'Airbnb Cereal App Bold';
-      font-style: normal;
-      font-weight: normal;
-      src: local('Airbnb Cereal App Bold'), url(${AirbnbCerealBold}) format('woff');
-    }
-
-    @font-face {
-      font-family: 'Airbnb Cereal App Extra Bold';
-      font-style: normal;
-      font-weight: normal;
-      src: local('Airbnb Cereal App Extra Bold'), url(${AirbnbCerealExtraBold}) format('woff');
-    }
-
-    @font-face {
-      font-family: 'Airbnb Cereal App Black';
-      font-style: normal;
-      font-weight: normal;
-      src: local('Airbnb Cereal App Black'), url(${AirbnbCerealBlack}) format('woff');
-    }
-
-
-  html, body {
-    font-family: 'Airbnb Cereal App Book', sans-serif;
-    color: rgb(34, 34, 34)
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-}
-`;
+import GlobalFonts from '../assets/fonts/GlobalFonts';
 
 class Booking extends React.Component {
   constructor() {
@@ -75,7 +17,7 @@ class Booking extends React.Component {
   render() {
     return (
       <>
-        <GlobalStyle />
+        <GlobalFonts />
         <h1>Hello World from Inside the Booking Component! Testing Again!</h1>
         <Widget />
         <Calendar />
