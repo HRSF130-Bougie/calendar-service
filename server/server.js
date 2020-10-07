fb9b1818f4ae97788ed4e8626e7b685751395abc
Elana Cohen-Khani <elanadev@gmail.com>
2020-10-07 14:30:39 -0700

    Add cors

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const orderRouter = require('./router.js');
 // eslint-disable-next-line no-unused-vars
@@ -8,6 +9,7 @@ const db = require('../database/connectToDatabaseLocal.js');
 
 const app = express();
 const port = 3002;
+app.use(cors());
 
 app.use(bodyParser.json());
 
