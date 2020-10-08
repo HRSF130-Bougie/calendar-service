c84c9f1adb012bcb109882815653b21bc0cca8d5
Elana Cohen-Khani <elanadev@gmail.com>
2020-10-07 23:38:22 -0700

    Removed font:inherit from reset.css

--- server/router.js
+++ server/router.js
@@ -10,7 +10,7 @@ router.route('/listings/').get((req, res) => {
 
 router.route('/listing/:listingId').get((req, res) => {
   const { listingId } = req.params;
-
+  console.log('hit me')
   schema.Listing.findOne({ listing_id: listingId })
     .then((listing) => res.status(200).send(listing))
     .catch((err) => res.status(400).send(`Error: ${err}`));
