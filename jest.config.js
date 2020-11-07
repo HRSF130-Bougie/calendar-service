e6281fdb8e271f51cecb919b84cde564078590e2
Elana Cohen-Khani <elanadev@gmail.com>
2020-11-07 00:34:17 -0800

    Added tests for "WidgetHeader"

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,16 @@ module.exports = {
   clearMocks: true,
   coverageDirectory: 'coverage',
   moduleFileExtensions: ['js', 'jsx'],
+  moduleNameMapper: {
+    '^image![a-zA-Z0-9$_-]+$': 'GlobalImageStub',
+    '^[./a-zA-Z0-9$_-]+\\.png$': '<rootDir>/RelativeImageStub.js',
+    'module_name_(.*)': '<rootDir>/substituted_module_$1.js',
+    'assets/(.*)': [
+      '<rootDir>/images/$1',
+      '<rootDir>/photos/$1',
+      '<rootDir>/recipes/$1',
+    ],
+  },
   setupFilesAfterEnv: ['jest-enzyme'],
   testEnvironment: 'enzyme',
   testEnvironmentOptions: { enzymeAdapter: 'react16' },
