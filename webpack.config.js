ad54818ad7da51258b14f7eb1fa988a94dc71b57
Elana Cohen-Khani <elanadev@gmail.com>
2020-11-12 00:51:58 -0800

    Fix entry point in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const config = {
   entry: [
-    '.client/src/index.jsx',
+    './client/src/index.jsx',
   ],
   output: {
     path: path.resolve(__dirname, './client/dist'),
