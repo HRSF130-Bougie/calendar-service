b84db65bbccc85901079270f303f3ed6e8d0a2a1
Elana Cohen-Khani <elanadev@gmail.com>
2020-11-13 23:08:02 -0800

    Add compression to app.use

--- server/server.js
+++ server/server.js
@@ -1,15 +1,17 @@
 /* eslint-disable no-console */
+const compression = require('compression');
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const router = require('./router.js');
 // eslint-disable-next-line no-unused-vars
-const db = require('../database/connectToDatabaseLocal.js');
+const db = require('../database/connectToDatabaseCompose.js');
 
 const app = express();
 const port = 3002;
 app.use(cors());
+app.use(compression());
 
 app.use(bodyParser.json());
 
